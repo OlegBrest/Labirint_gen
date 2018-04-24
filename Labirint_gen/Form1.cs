d363130f1bf150783d5cdbac28088eef743f396b
OlegBrest <pop457045@mail.ru>
2018-04-24 08:04:03 +0300

    now it seeding

--- Labirint_gen/Form1.cs
+++ Labirint_gen/Form1.cs
@@ -13,6 +13,7 @@ namespace Labirint_gen
     public partial class Main_Form : Form
     {
         public Bitmap main_scr;
+        private string old_seed = "";
         public Main_Form()
         {
             InitializeComponent();
@@ -23,20 +24,22 @@ namespace Labirint_gen
             int width = Convert.ToInt32(this.X_txtbx.Text);
             int height = Convert.ToInt32(this.Y_txtbx.Text);
             int diggers = Convert.ToInt32(this.digs_txtbx.Text);
-            if ((this.pictureBox.Image == null) || (width!=this.pictureBox.Image.Width) || (height != this.pictureBox.Image.Height))
+            string seed = this.seed_textbx.Text;
+            if ((this.pictureBox.Image == null) || (width!=this.pictureBox.Image.Width) || (height != this.pictureBox.Image.Height)||(!old_seed.Equals(seed)))
             {
                 main_scr = new Bitmap(width, height);
                 Graphics gr = default(Graphics);
                 gr = Graphics.FromImage(main_scr);
                 Rectangle rect = new Rectangle(0, 0, width, height);
                 gr.FillRectangle(Brushes.Green, rect);
+                old_seed = seed;
             }
             else main_scr = (Bitmap)this.pictureBox.Image;
             
             this.pictureBox.Image = main_scr;
+            digger dgr = new digger(main_scr, this.pictureBox, this.seed_textbx.Text);
             for (int i = 0; i < diggers; i++)
             {
-                digger dgr = new digger(main_scr, this.pictureBox);
                 main_scr = dgr.dig_it();
             }
             this.pictureBox.Image = main_scr;
--- Labirint_gen/digger.cs
+++ Labirint_gen/digger.cs
@@ -14,16 +14,29 @@ namespace Labirint_gen
         int cur_x_pos = 0;
         int cur_y_pos = 0;
         PictureBox pictureBox;
+        string[] seed;
+        Random[] random;
 
-        public digger (Bitmap bitmap, PictureBox pb)
+        // constructor
+        public digger(Bitmap bitmap, PictureBox pb, string sd)
         {
-            Random random = new Random();
+            seeding(sd);
+            this.random = new Random[1];
+            this.random[0] = new Random();
+            if (seed.Length > 0)
+            {
+                this.random = new Random[seed.Length];
+                for (int i = 0; i < seed.Length; i++)
+                {
+                   this.random[i] = new Random(StrToInt(seed[i]));
+                }
+            }
             this.Work_Bitmap = bitmap;
             this.pictureBox = pb;
-            this.cur_x_pos = random.Next(bitmap.Width / 2)+ (bitmap.Width / 4);
-            this.cur_y_pos = random.Next(bitmap.Height / 2) + (bitmap.Height / 4);
+            
         }
 
+        //refreshing screen
         public void Screen_update(bool update = true)
         {
             Color clr = this.Work_Bitmap.GetPixel(cur_x_pos, cur_y_pos);
@@ -43,24 +56,32 @@ namespace Labirint_gen
             }
             if (B >= G) B += step;
             Color clr2 = Color.FromArgb(255, R, G, (B > 255 ? 255 : B));
-            this.Work_Bitmap.SetPixel(cur_x_pos, cur_y_pos,clr2);
+            this.Work_Bitmap.SetPixel(cur_x_pos, cur_y_pos, clr2);
             this.pictureBox.Image = this.Work_Bitmap;
-            if(update)this.pictureBox.Update();
+            if (update) this.pictureBox.Update();
         }
 
+        //start digger for tonelling
         public Bitmap dig_it()
         {
-            Random random = new Random();
+            /* Random [] random = new Random[seed.Length];
+             for (int i = 0; i <seed.Length;i++)
+             {
+                 random[i] = new Random(StrToInt(seed[i]));
+             }*/
             
+
             int width = this.Work_Bitmap.Width;
             int height = this.Work_Bitmap.Height;
+            this.cur_x_pos = NextInt(width / 2, this.random) + (width / 4);
+            this.cur_y_pos = NextInt(height / 2, this.random) + (height / 4);
             int shags = 0;
-            
-            
+
+
             while ((this.cur_x_pos > 0) && (this.cur_x_pos < width) &&
                 (this.cur_y_pos > 0) && (this.cur_y_pos < height))
             {
-                int napr = random.Next(4);
+                int napr = NextInt(4, this.random);
                 if (napr == 0) this.cur_x_pos++;
                 if (napr == 1) this.cur_y_pos++;
                 if (napr == 2) this.cur_x_pos--;
@@ -71,19 +92,55 @@ namespace Labirint_gen
                     int updating = shags % 10000;
                     Screen_update(updating == 0);
                     if (updating == 0) shags = 0;
-                     shags++;
+                    shags++;
                 }
             }
-            for (int i = 0; i < (width * height / 4);i++)
+            for (int i = 0; i < (width * height / 4); i++)
             {
-                this.cur_x_pos = random.Next(width);
-                this.cur_y_pos = random.Next(height);
+                this.cur_x_pos = NextInt(width, this.random);
+                this.cur_y_pos = NextInt(height, this.random);
                 int updating = shags % 10000;
                 Screen_update(updating == 0);
                 if (updating == 0) shags = 0;
                 shags++;
             }
             return this.Work_Bitmap;
+        }
+
+        //fill array by seed
+        public void seeding(string sd)
+        {
+            int array_lenght = (int)Math.Round(0.4999999 + ((double)sd.Length / 4.0), 0);
+            this.seed = new string[array_lenght];
+            int curr_arr = 0;
+            for (int i = 0; i < sd.Length; i += 4)
+            {
+                this.seed[curr_arr] = sd.Substring(i, (sd.Length - i) >= 4 ? 4 : sd.Length - i);
+                curr_arr++;
+            }
+        }
+
+        // convert string value to integer
+        public int StrToInt(string str)
+        {
+            int result = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                result += (int) (((char)str[i]) * Math.Pow(2, i));
+            }
+            return result;
+        }
+
+        // getting array randoms middle
+        public int NextInt(int max_value, Random[] array, int center_x = 0, int center_y = 0)
+        {
+            int lenght = array.Length;
+            int [] result = new int[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                result [i] = array[i].Next(max_value);
+            }
+            return result[this.random[0].Next(lenght)];
         }
     }
 }
